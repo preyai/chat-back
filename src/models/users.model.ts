f710159339ab7b637b20aae7d4f190003e869502
preyai <preyaizman@gmail.com>
2022-04-22 23:32:44 +0300

    chats

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -10,7 +10,7 @@ export default function (app: Application): Model<any> {
   const mongooseClient: Mongoose = app.get('mongooseClient');
   const schema = new mongooseClient.Schema({
 
-    email: { type: String, unique: true, lowercase: true },
+    email: { type: String, lowercase: true, required: false },
     password: { type: String, required: true },
     nickname: { type: String, unique: true, required: true },
 
